4761d1863710bffad411daa1d51f1bf1a80782aa
u1233525 <u1233525@uel.ac.uk>
2017-07-12 14:24:56 +0100

    Very Very Very Basic AI Started
    ok

---
+++ Assets/MU_Scripts/MU_AIStates.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MU_AIStates : MonoBehaviour
+{
+    public string state;
+    public bool Bl_withinattackingdistance=false;
+    public bool Bl_withinchasingdistance = false;
+    public GameObject GO_Target;
+    public float ChaseRange;
+    public float AttackRange;
+    public NavMeshAgent MyNavmeshAgent;
+    // Use this for initialization
+    void Start()
+    {
+        state = "idle";
+        MyNavmeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void changestatesbasedondistance()
+    {
+        if(Vector3.Distance(transform.position,GO_Target.transform.position)<AttackRange)
+        {
+            Bl_withinattackingdistance = true;
+        }
+    }
+    void ChangeTheStates()
+    {
+        if(Bl_withinattackingdistance)
+        {
+            state = "Attack";
+        }
+    }
+    void DifferentStateReactions()
+    {
+        if(state=="Attack")
+        {
+
+        }
+    }
+}
